8b7ce92cc51f5ef23f496770dc416bc2e18ba991
K. Burak Özdemir <kburakozdemir@users.noreply.github.com>
2025-04-18 08:44:21 +0300

    Fix dark mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,27 @@
-import Aura from '@primevue/themes/aura';
+import Aura from "@primevue/themes/aura";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    '@primevue/nuxt-module',
-    '@nuxtjs/tailwindcss'
-  ],
+  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
   primevue: {
     options: {
       theme: {
-          preset: Aura
-      }
-    }
+        preset: Aura,
+        options: {
+          darkModeSelector: ".app-dark",
+        },
+      },
+    },
   },
   css: [
-    '@/assets/styles.scss',
+    "@/assets/styles.scss",
     //'@/assets/tailwind.css',
-  ],  
+  ],
   tailwindcss: {
     exposeConfig: true,
     viewer: true,
     // and more...
-  }
-})
+  },
+});
